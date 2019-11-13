20a3fd9e4246c5e5abb22ae25cf0e4e3957cc488
Mupat <dev@mupat.net>
2019-11-13 15:53:20 +0100

    Change specification to json

--- generate-publish.sh
+++ generate-publish.sh
@@ -6,7 +6,7 @@ oas generate > /dev/null 2>&1
 
 oas generate | \
 strip-bom | \
-jqf 'file => ({ "definition": JSON.stringify(file), "specification": "openapi/v3/yaml" })' | \
+jqf 'file => ({ "definition": JSON.stringify(file), "specification": "openapi/v3/json" })' | \
 curl \
   -X POST https://bump.sh/api/v1/docs/${BUMP_DOC_ID}/versions \
   -H "Content-Type: application/json" \

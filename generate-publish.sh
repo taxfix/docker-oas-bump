4b73f5c78ff81ce67b79ad8544682ac899ff97a1
Sami Ur Rehman <sami.ur-rehamn@taxfix.de>
2021-06-11 17:29:03 +0200

    feat: update generate-publish to leave behind api-docs.json

--- generate-publish.sh
+++ generate-publish.sh
@@ -3,8 +3,9 @@ if [ ! -f "$PWD/swagger.json" ]; then
     echo "specs not found"
     exit 0
 fi
-swagger-inline "$PWD/src/**/*.(py|ts|js)" "$PWD/*.(py|ts|js)" -b "$PWD/swagger.json" -f ".json" | \
-strip-bom | \
+swagger-inline "$PWD/src/**/*.(py|ts|js)" "$PWD/*.(py|ts|js)" -b "$PWD/swagger.json" -f ".json" > api-docs.json
+
+strip-bom api-docs.json \
 jqf 'file => ({ "definition": JSON.stringify(file), "specification": "openapi/v3/json" })' | \
 curl \
   -X POST https://bump.sh/api/v1/docs/${BUMP_DOC_ID}/versions \
